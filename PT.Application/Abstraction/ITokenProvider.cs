28140b6b62d43fd549a8a6e3ea16e55b9e00c0db
olarewaju muhammed <mohmzyo@gmail.com>
2024-11-21 11:19:46 +0100

    Implemented the generate token class

---
+++ PT.Application/Abstraction/ITokenProvider.cs
@@ -0,0 +1,14 @@
+﻿using PT.Domain.Entities.User;
+using System.Security.Claims;
+
+namespace PT.Application.Abstraction;
+
+public interface ITokenProvider
+    {
+        string GenerateRefreshToken();
+        string Create(IEnumerable<Claim> claims);
+        (string Token, string RefreshToken) Create(Users user);
+        ClaimsPrincipal GetPrincipalFromToken(string token);
+        string GenerateLinkToken();
+    }
+
---
+++ PT.Infratructure/Authentication/TokenProvider.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using PT.Application.Abstraction;
+using PT.Domain.Entities.User;
+using PT.Infratructure.Jwt;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PT.Infratructure.Authentication
+{
+    public class TokenProvider(IOptions<JwtSettings> jwtsettings) : ITokenProvider
+    {
+        private readonly JwtSettings _jwtsettings = jwtsettings.Value;
+        public string Create(IEnumerable<Claim> claims)
+        {
+
+        }
+
+        public (string Token, string RefreshToken) Create(Users user)
+        {
+            var secretKey = _jwtsettings.Key;
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var signiningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var subject = new ClaimsIdentity
+                (
+           [
+                 new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.Name,$"{user.FirstName} {user.LastName}")
+
+                ]);
+
+
+            var tokengenerator = new SecurityTokenDescriptor()
+            {
+                Issuer = _jwtsettings.Issuer,
+                Audience = _jwtsettings.Audience,
+                Expires = DateTime.Now.AddDays(_jwtsettings.TokenExpirationInMinutes),
+                Subject = subject,
+            };
+            var refreshTokengenerator = GenerateRefreshToken();
+            var handler = new JsonWebTokenHandler();
+            string token = handler.CreateToken(tokengenerator);
+            string refreshToken = handler.CreateToken(refreshTokengenerator);
+            return (token, refreshToken);
+    }
+
+        public string GenerateLinkToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal GetPrincipalFromToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
